7eb860d03c2ae4e5829cb230c83f70ba0e3f0f30
Jhoan Jesus Ortiz Sandoval <johanjesus500@gmail.com>
2023-12-17 22:07:28 -0500

    Static files and database folder deleted for better performance. Needed files are now downloaded using start services bash script, and each Dockerfile get its needed files from remote downloaded repo instead of saving them in this repo

--- Exercise2/app-295devops-travel/start_services.sh
+++ Exercise2/app-295devops-travel/start_services.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-docker compose up -d --build --remove-orphans --force-recreate
+repo_name="bootcamp-devops-2023"
+repo_url="https://github.com/roxsross/bootcamp-devops-2023.git"
+branch="clase2-linux-bash"
+
+find $repo_name &> /dev/null
+if [[ $? == 1 ]]; then
+    git clone $repo_url
+fi
+
+find $repo_name &> /dev/null
+if [[ $? == 0 ]]; then
+    cd $repo_name
+    git checkout $branch
+    git pull origin $branch
+fi
+
+docker compose up -d --build --remove-orphans
