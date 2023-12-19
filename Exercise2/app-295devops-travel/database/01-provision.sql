54c0ff4410a0701314748454112edf1a5441dd86
JHJORTIZ <JHJORTIZ@bancolombia.com.co>
2023-12-19 18:09:39 -0500

    docker compose settings decoupled using .env file for database config purposes

--- Exercise2/app-295devops-travel/database/01-provision.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS devopstravel;
-CREATE USER IF NOT EXISTS 'codeuser'@'localhost' IDENTIFIED BY 'codepass';
-GRANT ALL PRIVILEGES ON *.* TO 'codeuser'@'localhost';
-FLUSH PRIVILEGES;
-SHOW DATABASES;
