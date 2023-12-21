c569da4f2b3d2e450f139eed1e91194c09cf26c8
JHJORTIZ <JHJORTIZ@bancolombia.com.co>
2023-12-21 11:04:24 -0500

    Exercise 3 first commit :D

--- Exercise3/start_services.sh
+++ Exercise3/start_services.sh
@@ -2,24 +2,24 @@
 
 repo_name="bootcamp-devops-2023"
 repo_url="https://github.com/roxsross/bootcamp-devops-2023.git"
-branch="clase2-linux-bash"
+branch="ejercicio2-dockeriza"
 
 if [[ $(whoami) == "root" ]];
 then
     find $repo_name &> /dev/null
     if [[ $? == 1 ]]; then
         git clone $repo_url
-        chown -R "vagrant":"vagrant" $repo_name/
     fi
 
     find $repo_name &> /dev/null
     if [[ $? == 0 ]]; then
-        cd $repo_name
+        cd "$repo_name"
         git checkout $branch
         git pull origin $branch
+        cd "295words-docker/api/"
     fi
 
-    #docker compose up -d --build --force-recreate
+    docker compose up -d --build --force-recreate
 else
     echo "Root user is needed for execution ....."
 fi
