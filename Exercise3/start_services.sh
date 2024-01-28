d956d8daebf18588334e271349170ade76da8ffe
Jhoan Jesus Ortiz Sandoval <johanjesus500@gmail.com>
2024-01-27 23:05:23 -0500

    Command for unix conversion modified in Exercise2 and Exercise3. Exercise3's docker-compose now consumes variables from .env file. Little fixes to scripts

--- Exercise3/start_services.sh
+++ Exercise3/start_services.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-repo_name="bootcamp-devops-2023"
-repo_url="https://github.com/roxsross/bootcamp-devops-2023.git"
-branch="ejercicio2-dockeriza"
+clone_path=$1
+repo_name=$2
+repo_url=$3
+branch=$4
+owner="vagrant"
 
 if [[ $(whoami) == "root" ]];
 then
+    cd $clone_path
     find $repo_name &> /dev/null
     if [[ $? == 1 ]]; then
-        git clone $repo_url
+        git clone "$repo_url"
+        chown -R $owner:$owner $repo_name/
     fi
-
+    
     find $repo_name &> /dev/null
     if [[ $? == 0 ]]; then
-        cd "$repo_name"
+        cd $repo_name
         git checkout $branch
         git pull origin $branch
-        cd "295words-docker/api/"
     fi
-
+    cd $clone_path
     docker compose up -d --build --force-recreate
 else
     echo "Root user is needed for execution ....."
